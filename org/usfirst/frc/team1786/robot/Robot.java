59c37dc63375d1004e4caa206a1be8dcf252bb28
Dieter Brehm <Inkering@gmail.com>
2017-12-19 16:27:03 -0500

    Added basic mecanum functionality
    Added basic mecanum functionality. Just done by adding vectors to allow for a combination of translation and rotation

--- org/usfirst/frc/team1786/robot/Robot.java
+++ org/usfirst/frc/team1786/robot/Robot.java
@@ -1,78 +1,58 @@
 package org.usfirst.frc.team1786.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.MotorControl.CANTalon;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	CANTalon leftTalonFront = new CANTalon(1);
+	CANTalon leftTalonBack = new CANTalon(2);
+	CANTalon rightTalonFront = new CANTalon(3);
+	CANTalon rightTalonBack = new CANTalon(4);
+	
+	Joystick driverStick = new Joystick(0);
+	
+	public void mecanumDrive(double x, double y, double rotation, double Angle) {
+		double inputX = x;
+		double inputY = y;
+		
+		inputY = -inputY;
+		
+		double cosA = Math.cos(Angle * (3.14159 / 180.0));
+		double sinA = Math.sin(Angle * (3.14159 / 180.0));
+		
+		double[] rotated = new double[2];
+	    rotated[0] = inputX * cosA - inputY * sinA;
+	    rotated[1] = inputX * sinA + inputY * cosA;
+		
+		double rotatedX = rotated[0];
+		double rotatedY = rotated[1];
+		
+		leftTalonFront.set(rotatedX + rotatedY + rotation);
+		leftTalonBack.set(-rotatedX + rotatedY + rotation);
+		rightTalonFront.set(-rotatedX + );
+	}
+	
 	@Override
 	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
+		
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	@Override
 	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
 	@Override
 	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
-		}
 	}
+	
+	
 
-	/**
-	 * This function is called periodically during operator control
-	 */
 	@Override
 	public void teleopPeriodic() {
 	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
 	@Override
 	public void testPeriodic() {
 	}
